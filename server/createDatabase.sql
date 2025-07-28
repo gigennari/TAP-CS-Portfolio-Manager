c2b0f6ed0556eb7b2fb845f0da2587ba811133bd
gigennari <giov.gennari@hotmail.com>
2025-07-28 20:23:31 +0000

    yfinance wb

---
+++ server/createDatabase.sql
@@ -0,0 +1,17 @@
+
+--user table: 
+
+--accounts table:
+
+--intermediary portofolio table: acc fk, qty, price paid, fk stock 
+
+
+
+--stocks table -> updated with yfinance:
+--we'll have all stocks available for nyse and nasdaq and need to update rows once this is consulted
+
+
+
+--transactions per user:
+
+
